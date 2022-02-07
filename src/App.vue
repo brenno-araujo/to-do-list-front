cc3fb13bdf73adfcbafbe07f2134c3600bbe5638
brennoiboss <brenno.iboss@gmail.com>
2022-02-07 17:33:53 -0300

    feat: view todos

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="nav">
+  <div id="nav" class="text-center my-3">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <router-link :to="{name: 'Home'}">Home</router-link> |
     <router-link :to="{name: 'Todos.index'}">Todos</router-link>
   </div>
--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,7 +1,39 @@
 <template>
-    <h2>Lista de Tarefas</h2>
+    <div class="container">
+        <h2 class="text-center">Lista de Tarefas</h2>
+        <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Tarefa</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="name">
+            <div id="emailHelp" class="form-text">Nome da tarefa.</div>
+        </div>
+    </form>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.identify">
+            {{todo.name}}
+        </li>
+    </ul>
+    </div>
+    
 </template>
 
 <script>
+    import { ref } from 'vue' 
+    export default{
+        name: 'todos',
+        setup(){
+            const todos = [
+                {name:'tarefa 01', completed: true},
+                {name:'tarefa 02', completed: true},
+                {name:'tarefa 03', completed: false}
+            ]
 
+            const name = ref('default')
+        return {
+            todos,
+            name
+        }
+        }
+    }
 </script>
