af6b3de62198aaa81383c5b953efaba1a8b8257e
brennoiboss <brenno.iboss@gmail.com>
2022-02-08 10:06:07 -0300

    feat: loading

--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="container">
-        <h2 class="text-center">Lista de Tarefas</h2>
-        <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Tarefa</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="name">
-            <div id="emailHelp" class="form-text">Nome da tarefa.</div>
+        <h2 class="text-center">Tarefas</h2>
+        <div class="form-grop">
+            <form>
+                 <label for="exampleInputEmail1" class="form-label">Tarefa</label>
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Nome da tarefa" v-model="name">
+                    <button class="btn btn-primary">Add</button>
+                </div>
+            </form>
         </div>
-    </form>
+
+        <div v-show="loading" class="text-center">
+            <div class="spinner-border" role="status">
+                <span class="sr-only"></span>
+            </div>
+        </div>
 
     <ul class="list-group">
         <li v-for="todo in todos" :key="todo.identify" class="list-group-item">
@@ -27,14 +35,20 @@
         name: 'todos',
             setup() {
                 const todos = ref([])
-                onMounted(() => {
+
+                const loading = ref(false)
+
+                onMounted(async() => {
+                    loading.value = true
                     TodoService.getAll()
                         .then(response => {
                             todos.value = response.data.data
                         })
                         .catch(error => console.log(error))
+                        .finally(() => loading.value = false)
                 })
                 return {
+                    loading,
                     todos
                 }
             }
